4bb376baac57a2da84a6662912606aef4a380a39
Pedro Lima <contact.pedrolima@gmail.com>
2022-07-14 16:34:29 -0300

    netflix footer finished

--- src/pages/clones/Netflix/components/NetflixHomePageFooter.tsx
+++ src/pages/clones/Netflix/components/NetflixHomePageFooter.tsx
@@ -7,6 +7,12 @@ export function NetflixHomePageFooter() {
             <div className="flex flex-col h-full w-3/5 gap-5">
                 <NetflixSocialLinks />
                 <NetflixContentLinks />
+                <div className="border-[#808080] border-2 w-[107px] h-[37px] flex items-center justify-center">
+                    <a href="#" >
+                        <p className="text-[#808080] text-[13px]">SERVICE CODE</p>
+                    </a>
+                </div>
+                <p className="text-[11px] text-[#808080]">{"© 1997-2021 Netflix, Inc.  {i-062d573a0ee099242}"}</p>
             </div>
         </footer>
     )
