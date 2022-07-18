39cf1a4ace113914c7e650a44949a57c9aeb123a
Pedro Lima <contact.pedrolima@gmail.com>
2022-07-18 17:30:08 -0300

    added arrow to content carret

--- src/pages/clones/Netflix/components/NetflixContentCarousel.tsx
+++ src/pages/clones/Netflix/components/NetflixContentCarousel.tsx
@@ -1,3 +1,4 @@
+import { CaretRight } from "phosphor-react";
 import { NetflixContentCard } from "./NetlixContentCard";
 
 
@@ -20,10 +21,14 @@ export function NetflixContentCarousel(props: NetflixContentCarouselProps) {
             <div className="flex flex-nowrap flex-row gap-[5px] h-auto w-fit">
                 {
                     props.images.map((image, index) => (
-                        <NetflixContentCard key={index} image={image}/>
+                        <NetflixContentCard key={index} image={image} />
                     ))
                 }
                 
+                <div className="absolute z-40 bg-black w-[256px] h-[162px] rounded-sm bg-opacity-50 right-0 flex hover:opacity-0 cursor-pointer">
+                    <CaretRight size={32} color="#ffffff" className="self-center ml-52"/>
+                </div>
+
 
             </div>
         </div>
