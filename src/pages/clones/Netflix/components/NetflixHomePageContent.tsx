6c7e8a289d7412a5ddff9fd7a5c972308586d540
Pedro Lima <contact.pedrolima@gmail.com>
2022-07-19 17:10:28 -0300

    corrected categories

--- src/pages/clones/Netflix/components/NetflixHomePageContent.tsx
+++ src/pages/clones/Netflix/components/NetflixHomePageContent.tsx
@@ -15,12 +15,12 @@ export function NetflixHomePageContent() {
         <div className="w-full h-auto">
             <NetflixHighlightedContent />
             <NetflixContentCarousel carouselType="standard" category="Popular" images={testArrayStandard} />
-            <NetflixContentCarousel carouselType="standard" category="Popular" images={testArrayStandard} />
-            <NetflixContentCarousel carouselType="standard" category="Popular" images={testArrayStandard} />
-            <NetflixContentCarousel carouselType="vertical" category="Popular" images={testArrayVertical} />
-            <NetflixContentCarousel carouselType="top10" category="Popular" images={testArrayTop10} />
-            <NetflixContentCarousel carouselType="standard" category="Popular" images={testArrayStandard} />
-            <NetflixContentCarousel carouselType="standard" category="Popular" images={testArrayStandard} />
+            <NetflixContentCarousel carouselType="standard" category="Keep Watching" images={testArrayStandard} />
+            <NetflixContentCarousel carouselType="standard" category="What's New" images={testArrayStandard} />
+            <NetflixContentCarousel carouselType="vertical" category="Netflix Originals" images={testArrayVertical} />
+            <NetflixContentCarousel carouselType="top10" category="Top 10" images={testArrayTop10} />
+            <NetflixContentCarousel carouselType="standard" category="Watch again" images={testArrayStandard} />
+            <NetflixContentCarousel carouselType="standard" category="With a letter" images={testArrayStandard} />
         </div>
     )
 }
