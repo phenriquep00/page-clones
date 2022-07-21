af56c5a429bf429e26048b6af1fdf44f681585fb
Pedro Lima <contact.pedrolima@gmail.com>
2022-07-21 17:58:04 -0300

    removed header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,15 +5,13 @@ import { SideBar } from "../components/SideBar"
 export function HomePage() {
     return (
         <div className="w-screen h-screen bg-gray-800">
-            <header>
-                <h1 className="text-center text-2xl">page_clones</h1>
-            </header>
+            
 
-            <div className="flex flex-row w-screen h-[96vh]">
+            <div className="flex flex-row w-screen h-screen">
                 <SideBar />
 
                 <Showcase>
-                    <h2>Cloned pages</h2>
+                    <h2>page_clones</h2>
                     <div className="flex flex-row gap-5 p-5">
                         <CloneCard name="Netflix" path="./clones/netflix-profile-page" img=""/>
                         <CloneCard name="Wordle" path="./clones/wordle" img=""/>
