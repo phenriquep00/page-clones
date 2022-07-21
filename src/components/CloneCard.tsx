bb1010bdfb5f93191a5b99d821be28e5bb8e1cb1
Pedro Lima <contact.pedrolima@gmail.com>
2022-07-21 17:55:33 -0300

    added animation to cards

--- src/components/CloneCard.tsx
+++ src/components/CloneCard.tsx
@@ -15,7 +15,7 @@ export function CloneCard(props: CloneCardProps) {
                     <img src={props.img} alt="clone image" className="w-fit h-fit"/>
 
                 </div>
-                <div className="flex h-1/6 bg-gray-700 justify-center content-center border-b-2 border-gray-900 rounded-bl-2xl items-center text-gray-300 group-hover:bg-gray-800 group-hover:text-gray-100">
+                <div className="flex h-1/6 bg-gray-700 justify-center content-center border-b-2 border-gray-900 rounded-bl-2xl items-center text-gray-300 group-hover:bg-gray-800 group-hover:text-gray-100 transition-colors duration-300">
                     {props.name}
                 </div>
 
